0573c12626e98367c61615c9480785eb1252a00e
Ross Hill <rosshill88@gmail.com>
2017-11-22 20:15:28 +0000

    exercise C parts 1 + 2 complete

--- week1-git-homework/arrays-hashes-homework.rb
+++ week1-git-homework/arrays-hashes-homework.rb
@@ -103,12 +103,13 @@ united_kingdom = [
     name: "England",
     population: 53010000,
     capital: "London"
-  }
+  },
 ]
 
 # united_kingdom[1][:capital] = "Cardiff"
 # p united_kingdom
-# united_kingdom[3][:name] = "Belfast"
+united_kingdom << {name: "Northern Ireland", capital: "Belfast", population: 1811000}
+p united_kingdom
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
