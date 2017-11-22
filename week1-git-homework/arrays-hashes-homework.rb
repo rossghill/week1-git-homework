b36841b0e640c3e88b975d0f5acd456264b8ec63
Ross Hill <rosshill88@gmail.com>
2017-11-22 20:46:51 +0000

    exercises complete

--- week1-git-homework/arrays-hashes-homework.rb
+++ week1-git-homework/arrays-hashes-homework.rb
@@ -91,27 +91,30 @@
 
 
 united_kingdom = [
-  {
-    name: "Scotland",
-    population: 5295000,
-    capital: "Edinburgh"
-  }, {
-    name: "Wales",
-    population: 3063000,
-    capital: "Swansea"
-  }, {
-    name: "England",
-    population: 53010000,
-    capital: "London"
-  },
+  {name: "Scotland",population: 5295000,capital:"Edinburgh"},
+  {name: "Wales",population: 3063000,capital: "Swansea"},
+  {name: "England",population: 53010000,capital: "London"},
 ]
 
-# united_kingdom[1][:capital] = "Cardiff"
-# p united_kingdom
-united_kingdom << {name: "Northern Ireland", capital: "Belfast", population: 1811000}
-p united_kingdom
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 # 3. Use a loop to print the names of all the countries in the UK.
 # 4. Use a loop to find the total population of the UK
+
+# ***MY_ANSWERS***
+# united_kingdom[1][:capital] = "Cardiff"
+# p united_kingdom
+united_kingdom << {name: "Northern Ireland", capital: "Belfast", population: 1811000}
+
+# for country in united_kingdom
+#   p "#{country[:name]}"
+# end
+
+total_population = 0
+
+for country in united_kingdom
+  total_population += country[:population]
+end
+
+p total_population  
